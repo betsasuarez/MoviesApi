adaf7a901a720002a920f31c1590afbeb72783b2
CORREA <betsabe.correa@tecnologicoliceocristiano.edu.ec>
2024-02-19 20:52:38 -0500

    Agregar archivos de proyecto.

---
+++ Models/Artista.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesApi.Models
+{
+    public class Artista
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+
+    }
+}
---
+++ Models/ArtistaPelicula.cs
@@ -0,0 +1,14 @@
+﻿namespace MoviesApi.Models
+{
+    public class ArtistaPelicula
+    {
+        public int Id { get; set; }
+
+        public int AristaId { get; set; }
+
+        public Artista   Artista { get; set; }
+
+        public int PeliculaId { get; set; }
+        public Pelicula Pelicula { get; set; }
+    }
+}
